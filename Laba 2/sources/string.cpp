4f7c2ca743b0658421279facc90c71c89adf888f
ofrode <ofrode.nn@gmail.com>
2025-09-30 02:09:11 +0300

    Check Security

--- Laba 2/sources/string.cpp
+++ Laba 2/sources/string.cpp
@@ -4,17 +4,18 @@ using namespace std;
 
 String::String() : str(nullptr), length(0) {}
 
-String::String(const char *s)
+String::String(const char *s) : length(strlen(s))
 {
-    length = strlen(s);
     str = new char[length + 1];
-    strcpy(str, s);
+    strncpy(str, s, length);
+    str[length] = '\0';
 }
 
 String::String(const String &other) : length(other.length)
 {
     str = new char[length + 1];
-    strcpy(str, other.str);
+    strncpy(str, other.str, length);
+    str[length] = '\0';
 }
 
 String &String::operator=(const String &other)
@@ -22,10 +23,10 @@ String &String::operator=(const String &other)
     if (this != &other)
     {
         delete[] str;
-
         length = other.length;
         str = new char[length + 1];
-        strcpy(str, other.str);
+        strncpy(str, other.str, length);
+        str[length] = '\0';
     }
     return *this;
 }
@@ -35,13 +36,13 @@ String &String::operator+=(const String &other)
     size_t newLength = length + other.length;
     auto *newStr = new char[newLength + 1];
 
-    strcpy(newStr, str);
-    strcat(newStr, other.str);
+    strncpy(newStr, str, length);
+    strncpy(newStr + length, other.str, other.length + 1);
+    newStr[newLength] = '\0';
 
     delete[] str;
     str = newStr;
     length = newLength;
-
     return *this;
 }
 
